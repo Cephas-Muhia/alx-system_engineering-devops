95af186412860b8662f2df79302c0b60f4453291
Cephas-Muhia <Cephaschronixx@gmail.com>
2024-04-19 17:34:50 +0300

    instal a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-# installing a package
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
-
+# Using Puppet, install flask from pip3
+# package { 'flask':
+#   ensure   => '2.1.0',
+#     provider => 'pip3',
+#     }
